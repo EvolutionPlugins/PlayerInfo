b431803cc8aa85fc254bcffc0105fcb2ab056189
DiFFoZ <vama.ru472159472159@gmail.com>
2020-05-10 23:38:16 +0700

    Clean up code

---
+++ Player Info/Commands/PInfoCommand.cs
@@ -0,0 +1,67 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerInfo
+{
+    public class PInfoCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "pinfo";
+
+        public string Help => "Информация об игроке";
+
+        public string Syntax => "/pinfo <nickname>";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string> { "pinfo" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            var player = (UnturnedPlayer)caller;
+
+            if (command.Length == 0)
+            {
+                UnturnedChat.Say(caller, Syntax, Color.yellow);
+                return;
+            }
+
+            var plugin = PlayerInfo.Instance;
+
+            var otherplayer = UnturnedPlayer.FromName(command[0]);
+            if (otherplayer == null)
+            {
+                UnturnedChat.Say(player, plugin.Translate("PlayerNotFound"), Color.red);
+                return;
+            }
+
+            #region Vars
+
+            var nickname = otherplayer.DisplayName;
+            var steamNmae = otherplayer.SteamName;
+            var csteamId = otherplayer.CSteamID.m_SteamID;
+            var steamGroup = otherplayer.SteamGroupID.m_SteamID;
+            var ip = otherplayer.IP;
+            var ping = otherplayer.Ping;
+            var health = otherplayer.Health;
+            var hungry = otherplayer.Hunger;
+            var water = otherplayer.Thirst;
+            var poison = otherplayer.Infection;
+            var energy = otherplayer.Stamina;
+            var oxygen = otherplayer.Player.life.oxygen;
+            var experience = otherplayer.Experience;
+            var isBleeding = otherplayer.Bleeding;
+            var isBroken = otherplayer.Broken;
+
+            #endregion Vars
+
+            UnturnedChat.Say(player, plugin.Translate("Info_Player0", nickname, steamNmae, csteamId, steamGroup, ip, ping), Color.magenta);
+            UnturnedChat.Say(player, plugin.Translate("Info_Player1", health, hungry, water, poison), Color.magenta);
+            UnturnedChat.Say(player, plugin.Translate("Info_Player2", energy, oxygen, experience, isBleeding, isBroken), Color.magenta);
+        }
+    }
+}
---
+++ Player Info/PlayerInfo.cs
@@ -0,0 +1,32 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using System;
+
+namespace PlayerInfo
+{
+    public class PlayerInfo : RocketPlugin
+    {
+        public static PlayerInfo Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("Made with <3 by Evolution Plugins", ConsoleColor.Cyan);
+            Logger.Log("https://vk.com/evolutionplugins", ConsoleColor.Cyan);
+        }
+
+        protected override void Unload()
+        {
+            Instance = null;
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList
+        {
+            {"PlayerNotFound", "Игрок не найден" },
+            { "Info_Player0", "Имя в игре: {0}. Имя в Steam: {1}. SteamID64: {2}. SteamID64Group: {3}. IP: {4}. Пинг: {5}" },
+            { "Info_Player1", "Здоровье: {0}. Голод: {1}. Жажда: {2}. Радиация: {3}." },
+            { "Info_Player2", "Энергия: {0}. Кислород: {1}. Опыт: {2}. Кровотечение?: {3}. Кость сломана?: {4}" }
+        };
+    }
+}
